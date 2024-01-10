34ffa5e269b0a74cdacd62bbd0018d7af5f886fa
birukmk <birukawimekonnen@gmail.com>
2024-01-11 01:30:23 +0300

    clean up

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,6 +1,6 @@
 export const errorHandler = (statusCode, message) => {
   const error = new Error();
   error.statusCode = statusCode;
-  error.message = message;
+  error.stack = new Error().stack; 
   return error;
 };
