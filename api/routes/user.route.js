21f5941441f62a968657a9f86897c62cdf6dd010
birukmk <birukawimekonnen@gmail.com>
2024-01-11 01:20:37 +0300

    change to readability and maintainability

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,20 +1,15 @@
 import express from 'express';
-import {  test, updateUser, deleteUser,getUserListings,getUser } from '../controllers/user.controller.js';
+import {  test, updateUser, deleteUser, getUserListings, getUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
-
-
-
 
 const router = express.Router();
 
-router.get('/test', test);
-router.post('/update/:id', verifyToken, updateUser)
-router.delete('/delete/:id', verifyToken, deleteUser)
-router.get ('/listings/:id', verifyToken,getUserListings)
-router.get ('/:id', verifyToken,getUser)
+router.route('/:id')
+  .get(verifyToken, getUser)
+  .delete(verifyToken, deleteUser)
+  .post(verifyToken, updateUser);
 
-export default router;
+router.get('/listings/:id', verifyToken, getUserListings);
+router.post('/update/:id', verifyToken, updateUser);
 
-
-
-// 1 19
+// improve readability and maintainability of the code while preserving the same output and functionality.
